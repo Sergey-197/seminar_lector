18275a4ae6e9ad07ae1af4d0ad9a5725e5398b8b
Sergey <spumen35@gmail.com>
2024-01-26 15:21:56 +0300

    Сщздали lesson2

---
+++ lesson2/Program.cs
@@ -0,0 +1,69 @@
+﻿ // на вход принимает два числа и выводит, 
+ // какое число большее, а какое меньшее.
+ // first = 5
+ // second = 6
+//   int first; int second;
+  
+//   Console.WriteLine("Введите первое число ");
+//   first = int.Parse(Console.ReadLine()!);
+//   Console.WriteLine("Введите второе число ");
+//   second = int.Parse(Console.ReadLine()!);
+
+//   if (first > second)
+//   {
+//     Console.WriteLine("true");
+//   }
+// else
+// {
+// Console.WriteLine("false");
+// }
+
+
+
+// принимает на вход три числа и выдаёт максимальное из этих
+  
+ 
+
+Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число");
+int c = int.Parse(Console.ReadLine()!);
+
+ int max = a;
+       
+ if (max <b)
+ {
+        max = b;
+ }
+ if (max < c)
+ {
+ max = c;       
+ }
+Console.WriteLine(max);
+ 
+ 
+
+//на вход  принимает число (number), а на выходе выводит 
+// все чётные числа от 1 до number (включительно), 
+//после каждого числа должен быть знак пробела.
+
+// int number;
+// Console.WriteLine("Введите число number: ");
+// int.TryParse(Console.ReadLine(),out number);
+// if (args.Length >= 1)
+// {
+//     int number 
+// }
+
+
+
+
+
+
+
+
+ //на вход принимает число number и выводит, 
+ //является ли число чётным (делится ли оно на два без остатка).
+
